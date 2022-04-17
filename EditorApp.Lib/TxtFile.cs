544af8db8b914e68942dc738a79cdc0856eed29e
Starinin Andrey <starininandrey@gmail.com>
2022-04-17 15:23:11 +0300

    Добавление txt-файла

---
+++ EditorApp.Lib/TxtFile.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace EditorApp.Lib
+{
+    public class TxtFile : File, IFile
+    {
+        public TxtFile(string path, string content) : base(path, content) { }
+
+        public async Task Save()
+        {
+            await using var file = new StreamWriter(path, append: false);
+            await file.WriteAsync((string)content);
+        }
+
+        public async Task SaveAs(string path)
+        {
+            this.path = path;
+            await Save();
+        }
+
+        public async Task Open(string path)
+        {
+            this.path = path;
+            using var file = new StreamReader(path);
+            content = await file.ReadToEndAsync();
+        }
+
+        public void Create(string path)
+        {
+            this.path = path;
+            content = string.Empty;
+        }
+    }
+}
