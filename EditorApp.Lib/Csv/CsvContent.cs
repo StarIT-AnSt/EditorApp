7214b728158473eb3713302d8e4ff4872e0dcb68
Starinin Andrey <starininandrey@gmail.com>
2022-05-22 15:46:31 +0300

    Добавление CSV

---
+++ EditorApp.Lib/Csv/CsvContent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EditorApp.Lib.Csv
+{
+    public class CsvContent
+    {
+        private List<string> _header;
+        private List<List<string>> _content;
+
+        public CsvContent()
+        {
+            _header = new List<string>();
+            _content = new List<List<string>>();
+        }
+
+        public void Parser(string str)
+        {
+            var lines = str.Split("\r\n");
+
+            _header = lines[0].Split(';').ToList();
+            
+            for (int i = 1; i < lines.Length; i++)
+            {
+                _content.Add(lines[i].Split(';').ToList());
+            }
+        }
+
+        public override string ToString()
+        {
+            var temp = new StringBuilder();
+
+            foreach (var str in _header)
+            {
+                temp.Append($"{str};");
+            }
+            temp.Append("\r\n");
+
+            foreach (var str in _content)
+            {
+                foreach (var item in str)
+                {
+                    temp.Append($"{item};");
+                }
+                temp.Append("\r\n");
+            }
+
+            return temp.ToString();
+        }
+    }
+}
---
+++ EditorApp.Lib/Csv/CsvFile.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using EditorApp.Lib.Abstract;
+using File = EditorApp.Lib.Abstract.File;
+
+namespace EditorApp.Lib.Csv
+{
+    public class CsvFile : File, IFile
+    {
+        public CsvFile(string path, object content) : base(path, content) { }
+
+        public async Task Save()
+        {
+            await using var file = new StreamWriter(path, append: false);
+            await file.WriteAsync(content.ToString());
+        }
+
+        public async Task SaveAs(string path)
+        {
+            this.path = path;
+            await Save();
+        }
+
+        public async Task Open(string path)
+        {
+            this.path = path;
+            using var file = new StreamReader(path);
+            var temp = await file.ReadToEndAsync();
+            var tempContent = new CsvContent();
+            tempContent.Parser(temp);
+            content = tempContent;
+        }
+
+        public void Create(string path)
+        {
+            this.path = path;
+            content = new CsvContent();
+        }
+    }
+}
